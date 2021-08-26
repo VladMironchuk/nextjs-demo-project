e31382d61c12c148dc01a6ec51a1dc47dabe5099
Vlad <59253330+VladMironchuk@users.noreply.github.com>
2021-08-26 22:04:58 +0300

    Add files via upload

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+const handler = async (req, res) => {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://vladmiron:a8J3cuvQtuGWPZFp@cluster0.6mfby.mongodb.net/meetups"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    await client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+};
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+
+const HomePage = (props) => {
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name='description'
+          content='Browse a huge list of highly active React meetups!'
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://vladmiron:a8J3cuvQtuGWPZFp@cluster0.6mfby.mongodb.net/meetups"
+  );
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  await client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
